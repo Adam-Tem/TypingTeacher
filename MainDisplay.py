e08c95888df051f2bfce4ddadadd3d9d55611b68
Adam Templeton <2567437T@student.gla.ac.uk>
2022-12-10 13:05:41 +0000

    Added functionality to show prior and next letter, also typed this comment using the practice as best as I could!

--- MainDisplay.py
+++ MainDisplay.py
@@ -8,38 +8,52 @@ fingers_to_keys = {"L Pinky":["q", "a", "z"], "L Ring": ["s","w","x"],
     "R Index": ["y", "u", "h", "j", "n", "m"], "R Middle": ["i", "k"],
     "R Ring": ["o", "l"], "R Pinky": ["p"]}
 
-
-
 def generateRandomChar(event):
     streak = int(lbl_streak["text"][-1])
-    if(event.char == letter_display["text"]):
-        new_val = random.choice(string.ascii_lowercase)
-        new_letter.set(new_val)
+    if(event.char == curr_letter["text"]):
+        old_letter.set(current_letter.get())
+        current_letter.set(new_letter.get())
+        curr_val = current_letter.get()
+        new_letter.set(random.choice(string.ascii_lowercase))
+
         for finger, keys in fingers_to_keys.items():
-            if new_val in keys:
+            if curr_val in keys:
                 finger_to_use.config(text=finger)
-        letter_display.config(text=new_letter.get())
+        prior_letter.config(text=old_letter.get())
+        curr_letter.config(text=current_letter.get())
+        next_letter.config(text=new_letter.get())
         lbl_streak.config(text="Streak: " + str(streak + 1))
     else:
         lbl_streak.config(text="0")
 
 window = tk.Tk()
 
-title = tk.Label(text="Typing Teacher", fg="white", bg="#8eb5f5", font=("Arial", 20))
-title.pack(fill=tk.BOTH, expand=1)
+title = tk.Label(text="Typing Teacher", fg="black", bg="#8eb5f5", font=("Arial", 20))
+title.pack(fill=tk.BOTH,expand=True)
 
 lbl_streak = tk.Label(text = "Streak: 0", height = 1, font = 14)
-lbl_streak.pack(fill=tk.BOTH)
+lbl_streak.pack(fill=tk.BOTH,expand=True)
 
+old_letter = tk.StringVar()
+old_letter.set("...")
+current_letter = tk.StringVar()
+current_letter.set("a")
 new_letter = tk.StringVar()
-new_letter.set("a")
+new_letter.set("b")
 
-letter_display = tk.Label( text=new_letter.get(), fg="white", bg="black", font=("Times", 40))
+letters_frame = tk.Frame(master=window)
 
-letter_display.pack(fill=tk.BOTH, expand=1)
+prior_letter = tk.Label(letters_frame, text=old_letter.get(), fg="#b5b5b5", bg="black", font=("Times", 20))
+curr_letter = tk.Label(letters_frame, text=current_letter.get(), fg="white", bg="black", font=("Times", 40))
+next_letter = tk.Label(letters_frame, text=new_letter.get(), fg="#b5b5b5", bg="black", font=("Times", 20))
+
+letters_frame.pack(fill=tk.BOTH, expand=True)
+prior_letter.pack(fill=tk.BOTH,expand=True, side=tk.LEFT)
+curr_letter.pack(fill=tk.BOTH,expand=True, side=tk.LEFT)
+next_letter.pack(fill=tk.BOTH,expand=True, side=tk.LEFT)
 
 finger_to_use = tk.Label( text="L Pinky", fg="#d1d1d1", bg="black", font=("Arial", 15))
-finger_to_use.pack(fill=tk.BOTH, expand=1)
+finger_to_use.pack(fill=tk.BOTH,expand=True)
 
 window.bind('<KeyPress>', generateRandomChar)
 
